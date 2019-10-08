9d8bc2faa4419175c369c18a7b9e30c2cb6b9ff4
generous-vector-9581 <ojappah@gmail.com>
2019-10-08 01:13:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,16 +2,15 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 GENRES = []
-def genre = (genre)
-  @genre= genre
-  GENRES << genre
-end
-  def initialize(title)
+def initialize(title)
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+def genre = (genre)
+  @genre= genre
+  GENRES << genre
+end
 end
